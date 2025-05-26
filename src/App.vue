4050d0db314a5933e29d343afb9987db4a89064e
yelodia <yelodia@gmail.com>
2025-05-26 03:45:06 +0300

    layout and first component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ActionButton from './components/ActionButton.vue'
+
+const handleSort = () => {
+  console.log('Sort clicked')
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ActionButton 
+      text="Sort"
+      icon="sort_asc"
+      subtext="Ascending" 
+      @click="handleSort"
+    />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import injectHtml from 'vite-plugin-html-inject'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    injectHtml()
+  ]
 })
